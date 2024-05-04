12b01e66a2e2b33c69de1f0c651da321f31b4dca
Akhil Krishna <akhilmavannoor@gmail.com>
2024-05-04 23:09:32 +0530

    added db

--- prediction.py
+++ prediction.py
@@ -1,5 +1,31 @@
 import google.generativeai as genai
+from datetime import datetime
 #rom prediction import personality_traits
+import sqlite3
+
+
+# Connect to SQLite database (or create it if it doesn't exist)
+conn = sqlite3.connect('complaints.db')
+cursor = conn.cursor()
+
+# Create table to store department complaints data
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS complaints (
+        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        dept_id INTEGER,
+        dept_name TEXT,
+        comp_status TEXT,
+        comp_data TEXT,
+        mob_no TEXT,
+        date_of_comp TEXT,
+        date_of_res TEXT
+    )
+''')
+
+
+
+
+
 
 genai.configure(api_key="AIzaSyCdjmY7jiH7U_Z7JBfgu-omA2rd3S7sG2g")
 # proceed here for api key  https://makersuite.google.com/app/apikey
@@ -43,3 +69,7 @@ if __name__ == '__main__':
         if i.isdigit():
             deptnum=int(i)
     print(f"Luna: Department number : {deptnum} and response is {response}")
+
+    # Commit changes and close connection
+    conn.commit()
+    conn.close()
